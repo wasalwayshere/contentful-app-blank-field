94d4982b0b1621a5a8240f27468c4114ffce236d
Your Name <you@example.com>
2021-06-15 05:07:10 +0000

    Intial

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,16 +1,57 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
+import React, { useEffect, useState } from 'react'
+import { TextInput } from '@contentful/forma-36-react-components';
 import { FieldExtensionSDK } from '@contentful/app-sdk';
+
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
 }
 
 const Field = (props: FieldProps) => {
-  // If you only want to extend Contentful's default editing experience
-  // reuse Contentful's editor components
-  // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component</Paragraph>;
+
+  const [value, setValue] = useState(props.sdk.field.getValue() || []);
+
+  const fieldValue = props.sdk.field.getValue();
+  const fieldName = props.sdk.field.id;
+  const entryMeta = props.sdk.entry.getSys();
+  const entryID = entryMeta.id;
+
+  // use contentful's builtin auto-resizer
+  useEffect(() => {
+    props.sdk.window.startAutoResizer();
+  });
+
+  // external change
+  useEffect(() => {
+    const detachExternalChangeHandler = () => props.sdk.field.onValueChanged(setValue)
+  });
+
+  const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value;
+    // console.log(value);
+    setValue( value );
+    if (value) {
+      await props.sdk.field.setValue(value)
+          .then((data) => {
+            // console.log(data);  // do something else
+          });
+    } else {
+      await props.sdk.field.removeValue();
+    }
+
+  }
+
+  return (
+      <TextInput
+          width="large"
+          type="text"
+          id="my-field"
+          testId="my-field"
+          value={value}
+          onChange={onChange}
+      />
+  );
+
 };
 
 export default Field;
